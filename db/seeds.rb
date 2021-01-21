e32110d216b7e069e96d3162b719a5d581c5a38c
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-01-21 20:34:59 +0100

    Simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Miraculous Flat in Paris',
+  address: '123 nous irons aux bois',
+  description: 'Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla',
+  price_per_night: 300,
+  number_of_guests: 12
+)
+
+Flat.create!(
+  name: 'Bye bye bye',
+  address: '456 cueillir des saucisses',
+  description: 'Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla',
+  price_per_night: 49,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'No such ideas',
+  address: '999 rue de la boustifaille',
+  description: 'Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla',
+  price_per_night: 29,
+  number_of_guests: 4
+)
